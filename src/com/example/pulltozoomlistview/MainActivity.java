7c5c5f326964e38d50e360da2dd69fd5eb0906e6
haitao <758277560@qq.com>
2015-03-30 17:47:11 +0800

    init

---
+++ src/com/example/pulltozoomlistview/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.pulltozoomlistview;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import com.example.pulltozoomlistview.adapter.MessageListAdapter;
+import com.example.pulltozoomlistview.util.DensityUtil;
+import com.example.pulltozoomlistview.view.PullToZoomListView;
+import com.example.pulltozoomlistview.view.PullToZoomListView.PullToZoomListViewListener;
+
+public class MainActivity extends Activity implements PullToZoomListViewListener{
+
+	int i = 0;
+	private  PullToZoomListView mListView;
+	private MessageListAdapter mAdapter;
+	private List<HashMap<String, Object>> listItem=new ArrayList<HashMap<String,Object>>();
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initListView();
+	}
+
+	 
+
+	 private void initListView() {
+		getData();
+		mListView=(PullToZoomListView) findViewById(R.id.listView01);
+		mAdapter = new MessageListAdapter(listItem,R.layout.news_list_item_view,getLayoutInflater(),MainActivity.this);
+
+		/*========================= 设置头部的图片====================================*/
+		mListView.getHeaderView().setScaleType(ImageView.ScaleType.CENTER_CROP);
+		mListView.getHeaderView().setImageResource(R.drawable.story_member_center_bg);
+		
+		/*========================= 设置头部的高度 ====================================*/
+		mListView.setmHeaderHeight(DensityUtil.dip2px(MainActivity.this, 260));
+
+		/*========================= 设置头部的的布局====================================*/
+		 View mHeaderView=getLayoutInflater().inflate(R.layout.layout_story_userinfo, null);
+		 mListView.getHeaderContainer().addView(mHeaderView);
+		 mListView.setHeaderView();
+		 mListView.setAdapter(mAdapter);
+		 mListView.setPullToZoomListViewListener(this);
+
+	}
+	
+
+	private List<HashMap<String, Object>> getData()	{
+		;
+		for(int i=0;i<3;i++)	{
+			HashMap<String,Object> map=new HashMap<String, Object>();
+			map.put("key", ""+i);
+			listItem.add(map);
+		}
+		return listItem; 
+	}
+	@Override
+	public void onLoadMore() {
+		i++;
+		if (i < 3) {
+
+			new Handler().postDelayed(new Runnable() {
+
+				@Override
+				public void run() {
+					getData();
+					mAdapter.notifyDataSetChanged();
+					mListView.setLoadFinish(false);
+				}
+			}, 3000);
+		} else if (i == 3) {
+			mListView.setLoadFinish(true);
+			new Handler().postDelayed(new Runnable() {
+
+				@Override
+				public void run() {
+					mListView.setLoadFinish(false);
+				}
+			}, 3000);
+		} else {
+			new Handler().postDelayed(new Runnable() {
+
+				@Override
+				public void run() {
+					getData();
+					mAdapter.notifyDataSetChanged();
+					mListView.setLoadFinish(false);
+				}
+			}, 3000);
+		}
+	}
+
+}
